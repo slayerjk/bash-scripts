bf09069dfd37b226f693544466d6f71d1f0efb72
slayerjk <slayer.jk@mail.ru>
2022-09-23 09:57:35 +0600

    +added  options; *cosmetics
    * modified (BOTH)- > PDIR option(one pid & one dir)
    + added DIR option(list of dirs)
    * minor dosmetics / mistypes fixes

--- log-lsof-by-pid.sh
+++ log-lsof-by-pid.sh
@@ -9,7 +9,7 @@ START_TIME=$SECONDS
 ### SCRIPT INFO ##
 ##################
 
-# Log-Lsof-by-Pid - this script purpose is to log all lsof outupt by PID
+# Log-Lsof-by-Pid - this script purpose is to log all lsof outupt(see OPTIONs)
 # By default script works for 2 minutes
 # pgrep - must be installed
 # lsof - must be installed
@@ -21,23 +21,25 @@ START_TIME=$SECONDS
 LOG_DIR=/tmp
 LOG_NAME=log-lsof_$(date +%F).log
 
+### LSOF SNAPSHOT ITNERVAL(sec), OVERALL TIME TO RUN(sec)
 LSOF_INTERVAL=2
-TIME_TO_RUN=120
-
+TIME_TO_RUN=40
 
 ### LOGGING OPTIONS
-### VALID ARE: pid, dir, both
+### VALID ARE: pid, dir, dirs, pdir
 ### pid: LOG ONLY PID RELATED
 ### dir: LOG ONLY DIRS RELATED
-### both: BOTH
-OPTION=dir
+### dirs: LIST OF DIRS
+### pdir: PID & DIR(one dir due to restrictions)
+OPTION=pid
 
 ### LOOKUP FOR PID(PID_NAME IS YOUR CHOICE)
-PID_NAME='wazuh-syscheckd'
+PROCESS_NAME='wazuh-syscheckd'
 
 ### DIRS TO INSPECT
 ### DIRECTORIES MUST EXIST!
-DIR=/<your-dir>
+DIR=/var/ossec
+DIRS=(/usr/bin /var/ossec /usr/lib)
 
 #######################
 # DONT'T EDIT FURTHER #
@@ -52,15 +54,13 @@ date
 ELAPSED_TIME=$((SECONDS - START_TIME))
 
 function checking_pid () {
-  PID=$(pgrep "$PID_NAME")
+  PID=$(pgrep "$PROCESS_NAME")
   ### CHECK PID EXISTS
   if [ -z "$PID" ]; then
     echo "PID not found, exiting..."
     exit 1
   else
-    echo "PID found: $PID"
-    echo "Stating to log lsof output for $PID"
-    echo "When you've done, log is $LOG_DIR/$LOG_NAME"
+    echo "Starting to log lsof output"
   fi
 }
 
@@ -69,33 +69,52 @@ if [ "$OPTION" = "pid" ]; then
   checking_pid
   ### RUNNING LSOF WITH pid OPTON
   echo "Running script with PID option"
+  echo "PROCESS_NAME is "$PROCESS_NAME""
+  echo "PID is "$PID""
   while [ "$ELAPSED_TIME" -le "$TIME_TO_RUN" ]; do
     echo "=============="
     lsof -p "$PID"
-    echo "Elased time is: $((SECONDS - START_TIME))"
+    echo "Elapsed time is: $((SECONDS - START_TIME))"
     sleep $LSOF_INTERVAL
     ELAPSED_TIME=$((SECONDS - START_TIME))
   done
-### OPTION both(PID&DIR)
-elif [ "$OPTION" = "both" ]; then
+### OPTION pdir(PID&DIR)
+elif [ "$OPTION" = "pdir" ]; then
   checking_pid
   ### RUNNING LSOF WITH both OPTON
-  echo "Running script with both PID&DIR option"
+  echo "Running script with PID&DIR option"
+  echo "Running script with PID option"
+  echo "PROCESS_NAME is "$PROCESS_NAME""
+  echo "PID is "$PID""
+  echo "DIR is "$DIR""
   while [ "$ELAPSED_TIME" -le "$TIME_TO_RUN" ]; do
     echo "=============="
     lsof -p "$PID" +D "$DIR" -a
-    echo "Elased time is: $((SECONDS - START_TIME))"
+    echo "Elapsed time is: $((SECONDS - START_TIME))"
     sleep $LSOF_INTERVAL
     ELAPSED_TIME=$((SECONDS - START_TIME))
   done
-### OPTION dir
+#### OPTION dir
 elif [ "$OPTION" = "dir" ]; then
   ### RUNNING LSOF WITH dir OPTON
   echo "Running script with DIR option"
+  echo "DIR is "$DIR""
   while [ "$ELAPSED_TIME" -le "$TIME_TO_RUN" ]; do
     echo "=============="
     lsof +D "$DIR"
-    echo "Elased time is: $((SECONDS - START_TIME))"
+    echo "Elapsed time is: $((SECONDS - START_TIME))"
+    sleep $LSOF_INTERVAL
+    ELAPSED_TIME=$((SECONDS - START_TIME))
+  done
+## OPTION dirs
+elif [ "$OPTION" = "dirs" ]; then
+  ### RUNNING LSOF WITH dirs OPTION
+  echo "Running script with DIRS option"
+  echo "DIRS are "$DIRS""
+  while [ "$ELAPSED_TIME" -le "$TIME_TO_RUN" ]; do
+    echo "=============="
+    lsof +D "${DIRS[@]}"
+    echo "Elapsed time is: $((SECONDS - START_TIME))"
     sleep $LSOF_INTERVAL
     ELAPSED_TIME=$((SECONDS - START_TIME))
   done
